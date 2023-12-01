ed0b12cb18868c938869c5ef6fc0527927ba141e
Georgy Bokoev <goshik04@gmail.com>
2023-12-01 20:27:51 +0300

    Update

--- js/like-button.js
+++ js/like-button.js
@@ -1,66 +1,66 @@
-favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || [];
+favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || []
 
-updateFavoriteCount();
+updateFavoriteCount()
 
 document.addEventListener('DOMContentLoaded', () => {
-    let buttons_like = document.getElementsByClassName('like-container');
+	let buttons_like = document.getElementsByClassName('like-container')
 
-    for (let i = 0; i < buttons_like.length; i++) {
-        let button = buttons_like[i];
+	for (let i = 0; i < buttons_like.length; i++) {
+		let button = buttons_like[i]
 
-        let buttonId = button.dataset.id;
-        
-        let isLiked = localStorage.getItem('likeButton_' + buttonId);
-        
-        if (isLiked === 'true') {
-            button.style.backgroundColor = 'red';
-        }
+		let buttonId = button.dataset.id
 
-        button.addEventListener('click', function() {
-            toggleLike(button);
-            addToFavorites(button);
-        });
-    }
-});
+		let isLiked = localStorage.getItem('likeButton_' + buttonId)
+
+		if (isLiked === 'true') {
+			button.style.backgroundColor = 'red'
+		}
+
+		button.addEventListener('click', function () {
+			toggleLike(button)
+			addToFavorites(button)
+		})
+	}
+})
 
 function toggleLike(button) {
-    if (button.style.backgroundColor === '') {
-        button.style.backgroundColor = 'red';
-    } else {
-        button.style.backgroundColor = '';
-    }
+	if (button.style.backgroundColor === '') {
+		button.style.backgroundColor = 'red'
+	} else {
+		button.style.backgroundColor = ''
+	}
 
-    let buttonId = button.dataset.id;
+	let buttonId = button.dataset.id
 
-    let isLiked = button.style.backgroundColor === 'red';
+	let isLiked = button.style.backgroundColor === 'red'
 
-    localStorage.setItem('likeButton_' + buttonId, JSON.stringify(isLiked));
+	localStorage.setItem('likeButton_' + buttonId, JSON.stringify(isLiked))
 }
 
 function addToFavorites(button) {
-    let itemName = button.parentElement.querySelector(".description h2").innerText;
+	let itemName = button.parentElement.querySelector('.description h2').innerText
 
-    let itemIndex = favoriteItems.indexOf(itemName);
+	let itemIndex = favoriteItems.indexOf(itemName)
 
-    if (itemIndex === -1) {
-        favoriteItems.push(itemName);
-    } else {
-        let imageElement = button.closest('.image');
-        
-        if (imageElement) {
-            imageElement.style.display = 'none';
-        }
+	if (itemIndex === -1) {
+		favoriteItems.push(itemName)
+	} else {
+		let imageElement = button.closest('.image')
 
-        favoriteItems.splice(itemIndex, 1);
-    }
-    
-    updateFavoriteCount();
+		if (imageElement) {
+			imageElement.style.display = 'none'
+		}
 
-    localStorage.setItem('favoriteItems', JSON.stringify(favoriteItems));
+		favoriteItems.splice(itemIndex, 1)
+	}
+
+	updateFavoriteCount()
+
+	localStorage.setItem('favoriteItems', JSON.stringify(favoriteItems))
 }
 
 function updateFavoriteCount() {
-    let countElement = document.getElementById("favoriteCount");
-    
-    countElement.innerText = favoriteItems.length;
+	let countElement = document.getElementById('favoriteCount')
+
+	countElement.innerText = favoriteItems.length
 }
--- js/likes.js
+++ js/likes.js
@@ -1,133 +1,139 @@
-document.addEventListener('DOMContentLoaded', displayFavorites);
+document.addEventListener('DOMContentLoaded', displayFavorites)
 
 function displayFavorites() {
-    favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || [];
+	favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || []
 
-    let countElement = document.getElementById("favoriteCount");
-    
-    countElement.innerText = favoriteItems.length;
-    
-    let favoritesContainer = document.getElementById('gallery-container');
-    
-    favoriteItems.forEach(function(item) {
-        let imageContainer = document.createElement('div');
-        
-        imageContainer.classList.add('image');
-    
-        let image = document.createElement('img');
-        image.classList.add('image_i');
+	let countElement = document.getElementById('favoriteCount')
 
-        switch(item) {
-            case 'Замиокулькас':
-                image.setAttribute('src', 'images/543.750x0.jpg');
-                break;
-            case 'Антуриум':
-                image.setAttribute('src', 'images/anthurium-210827_1920.jpg');
-                break;
-            case 'Гипоэстес':
-                image.setAttribute('src', 'images/1-58.jpg');
-                break;
-            case 'Колеус':
-                image.setAttribute('src', 'images/1650526944_37-vsegda-pomnim-com-p-kak-viglyadit-tsvetok-koleus-foto-38.jpg');
-                break;
-            case 'Ктенанта':
-                image.setAttribute('src', 'images/f0bbfd30cc958545fb03fdc8b5b83735.jpg');
-                break;
-            case 'Венерина мухоловка':
-                image.setAttribute('src', 'images/8f85af9633aa75e818603a739e627c5b.jpeg');
-                break;
-            case 'Бегония':
-                image.setAttribute('src', 'images/f3ca7c9d996533932c528277011d390d.png');
-                break;
-            case 'Аглаонема':
-                image.setAttribute('src', 'images/382_2.jpg');
-                break;
-        }
+	countElement.innerText = favoriteItems.length
 
-        image.setAttribute('alt', item);
-    
-        let description = document.createElement('div');
-        description.classList.add('description');
-    
-        let heading = document.createElement('h2');
-        heading.textContent = item;
-    
-        let link = document.createElement('a');
+	let favoritesContainer = document.getElementById('gallery-container')
 
-        switch(item) {
-            case 'Замиокулькас':
-                link.setAttribute('href', 'zamiokulkas.html');
-                break;
-            case 'Антуриум':
-                link.setAttribute('href', 'anturium.html');
-                break;
-            case 'Гипоэстес':
-                link.setAttribute('href', 'gipoestes.html');
-                break;
-            case 'Колеус':
-                link.setAttribute('href', 'koleus.html');
-                break;
-            case 'Ктенанта':
-                link.setAttribute('href', 'ktenanta.html');
-                break;
-            case 'Венерина мухоловка':
-                link.setAttribute('href', 'muholovka.html');
-                break;
-            case 'Бегония':
-                link.setAttribute('href', 'begonia.html');
-                break;
-            case 'Аглаонема':
-                link.setAttribute('href', 'aglaonema.html');
-                break;
-        }
+	favoriteItems.forEach(function (item) {
+		let imageContainer = document.createElement('div')
 
-        let button = document.createElement('button');
-        button.textContent = 'Подробнее';
-        link.appendChild(button);
+		imageContainer.classList.add('image')
 
-        let likeContainer = document.createElement('button');
-        likeContainer.classList.add('like-container');
+		let image = document.createElement('img')
+		image.classList.add('image_i')
 
-        switch(item) {
-            case 'Замиокулькас':
-                likeContainer.setAttribute('data-id', '1');
-                break;
-            case 'Антуриум':
-                likeContainer.setAttribute('data-id', '2');
-                break;
-            case 'Гипоэстес':
-                likeContainer.setAttribute('data-id', '7');
-                break;
-            case 'Колеус':
-                likeContainer.setAttribute('data-id', '3');
-                break;
-            case 'Ктенанта':
-                likeContainer.setAttribute('data-id', '8');
-                break;
-            case 'Венерина мухоловка':
-                likeContainer.setAttribute('data-id', '6');
-                break;
-            case 'Бегония':
-                likeContainer.setAttribute('data-id', '4');
-                break;
-            case 'Аглаонема':
-                likeContainer.setAttribute('data-id', '5');
-                break;
-        }
-        
-        let likeButton = document.createElement('img');
-        likeButton.classList.add('like-button');
-        likeButton.setAttribute('src', 'images/heart_1.png');
-        likeButton.setAttribute('width', '16');
-        likeButton.setAttribute('height', '16');
+		switch (item) {
+			case 'Замиокулькас':
+				image.setAttribute('src', 'images/543.750x0.jpg')
+				break
+			case 'Антуриум':
+				image.setAttribute('src', 'images/anthurium-210827_1920.jpg')
+				break
+			case 'Гипоэстес':
+				image.setAttribute('src', 'images/1-58.jpg')
+				break
+			case 'Колеус':
+				image.setAttribute(
+					'src',
+					'images/1650526944_37-vsegda-pomnim-com-p-kak-viglyadit-tsvetok-koleus-foto-38.jpg'
+				)
+				break
+			case 'Ктенанта':
+				image.setAttribute('src', 'images/f0bbfd30cc958545fb03fdc8b5b83735.jpg')
+				break
+			case 'Венерина мухоловка':
+				image.setAttribute(
+					'src',
+					'images/8f85af9633aa75e818603a739e627c5b.jpeg'
+				)
+				break
+			case 'Бегония':
+				image.setAttribute('src', 'images/f3ca7c9d996533932c528277011d390d.png')
+				break
+			case 'Аглаонема':
+				image.setAttribute('src', 'images/382_2.jpg')
+				break
+		}
 
-        likeContainer.appendChild(likeButton);
-        description.appendChild(heading);
-        description.appendChild(link);
-        imageContainer.appendChild(image);
-        imageContainer.appendChild(description);
-        imageContainer.appendChild(likeContainer);
-    
-        favoritesContainer.appendChild(imageContainer);
-    });
+		image.setAttribute('alt', item)
+
+		let description = document.createElement('div')
+		description.classList.add('description')
+
+		let heading = document.createElement('h2')
+		heading.textContent = item
+
+		let link = document.createElement('a')
+
+		switch (item) {
+			case 'Замиокулькас':
+				link.setAttribute('href', 'zamiokulkas.html')
+				break
+			case 'Антуриум':
+				link.setAttribute('href', 'anturium.html')
+				break
+			case 'Гипоэстес':
+				link.setAttribute('href', 'gipoestes.html')
+				break
+			case 'Колеус':
+				link.setAttribute('href', 'koleus.html')
+				break
+			case 'Ктенанта':
+				link.setAttribute('href', 'ktenanta.html')
+				break
+			case 'Венерина мухоловка':
+				link.setAttribute('href', 'muholovka.html')
+				break
+			case 'Бегония':
+				link.setAttribute('href', 'begonia.html')
+				break
+			case 'Аглаонема':
+				link.setAttribute('href', 'aglaonema.html')
+				break
+		}
+
+		let button = document.createElement('button')
+		button.textContent = 'Подробнее'
+		link.appendChild(button)
+
+		let likeContainer = document.createElement('button')
+		likeContainer.classList.add('like-container')
+
+		switch (item) {
+			case 'Замиокулькас':
+				likeContainer.setAttribute('data-id', '1')
+				break
+			case 'Антуриум':
+				likeContainer.setAttribute('data-id', '2')
+				break
+			case 'Гипоэстес':
+				likeContainer.setAttribute('data-id', '7')
+				break
+			case 'Колеус':
+				likeContainer.setAttribute('data-id', '3')
+				break
+			case 'Ктенанта':
+				likeContainer.setAttribute('data-id', '8')
+				break
+			case 'Венерина мухоловка':
+				likeContainer.setAttribute('data-id', '6')
+				break
+			case 'Бегония':
+				likeContainer.setAttribute('data-id', '4')
+				break
+			case 'Аглаонема':
+				likeContainer.setAttribute('data-id', '5')
+				break
+		}
+
+		let likeButton = document.createElement('img')
+		likeButton.classList.add('like-button')
+		likeButton.setAttribute('src', 'images/heart_1.png')
+		likeButton.setAttribute('width', '16')
+		likeButton.setAttribute('height', '16')
+
+		likeContainer.appendChild(likeButton)
+		description.appendChild(heading)
+		description.appendChild(link)
+		imageContainer.appendChild(image)
+		imageContainer.appendChild(description)
+		imageContainer.appendChild(likeContainer)
+
+		favoritesContainer.appendChild(imageContainer)
+	})
 }
